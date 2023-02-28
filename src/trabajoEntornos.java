c12698961885e355c53b21b63a63dd83c7262c3c
Laura <misakuritapro1@hotmail.com>
2023-02-28 21:49:04 +0100

    Aqui guardamos la primera version

--- src/trabajoEntornos.java
+++ src/trabajoEntornos.java
@@ -4,15 +4,23 @@ public class trabajoEntornos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
-        int dato = teclado.nextInt();
-        int vector[] = new int[dato];
-        System.out.println("\nVector inicial hasta :" + dato);
+        int numero = teclado.nextInt();
+        int listaNumeros[] = new int[numero];
+        System.out.println("\nVector inicial hasta :" + numero);
+        imprimeTodos(listaNumeros);
+        listaNumeros = generarPrimos(numero);
+        System.out.println("\nVector de primos hasta:" + numero);
+        imprime(listaNumeros);
+    }
+
+    private static void imprimeTodos(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             if (i % 10 == 0) System.out.println();
             System.out.print(i + 1 + "\t");
         }
-        vector = generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:" + dato);
+    }
+
+    private static void imprime(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             if (i % 10 == 0) System.out.println();
             System.out.print(vector[i] + "\t");
@@ -32,29 +40,48 @@ public class trabajoEntornos {
             // Eliminar el 0 y el 1, que no son primos
             esPrimo[0] = esPrimo[1] = false;
             // Criba
-            for (i = 2; i < Math.sqrt(dim) + 1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j = 2 * i; j < dim; j += i)
-                        esPrimo[j] = false;
-                }
-            }
+            seleccionPrimos(dim, esPrimo);
             // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i = 0; i < dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
-            }
+            int cuenta = cuantosHay(dim, esPrimo);
             // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i = 0, j = 0; i < dim; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
-            }
+            int[] primos = getPrimos(dim, esPrimo, cuenta);
             return primos;
         } else { // max < 2
             return new int[0];
             // Vector vacío
+        }
+    }
+
+    private static int cuantosHay(int dim, boolean[] esPrimo) {
+        int i;
+        int cuenta = 0;
+        for (i = 0; i < dim; i++) {
+            if (esPrimo[i])
+                cuenta++;
+        }
+        return cuenta;
+    }
+
+    private static int[] getPrimos(int dim, boolean[] esPrimo, int cuenta) {
+        int j;
+        int i;
+        int[] primos = new int[cuenta];
+        for (i = 0, j = 0; i < dim; i++) {
+            if (esPrimo[i])
+                primos[j++] = i;
+        }
+        return primos;
+    }
+
+    private static void seleccionPrimos(int dim, boolean[] esPrimo) {
+        int i;
+        int j;
+        for (i = 2; i < Math.sqrt(dim) + 1; i++) {
+            if (esPrimo[i]) {
+                // Eliminar los múltiplos de i
+                for (j = 2 * i; j < dim; j += i)
+                    esPrimo[j] = false;
+            }
         }
     }
 }
